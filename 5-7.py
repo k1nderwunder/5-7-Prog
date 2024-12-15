dcce7e4018b7487befdeba9655129dfe3bc5a675
k1nderwunder <maxday202020@gmail.com>
2024-12-16 03:00:31 +0700

    Version 1
    Основные заглушки

---
+++ 5-7.py
@@ -0,0 +1,23 @@
+import random
+import string
+
+def main():
+    pass
+
+def input_data():
+    return "образец"
+
+def generate_random_text():
+    return "образец"
+
+def find_palindromes(text):
+    return "образец"
+
+def print_palindromes(palindromes):
+    pass
+
+def exit_program():
+    pass
+
+if __name__ == "__main__":
+    main()
